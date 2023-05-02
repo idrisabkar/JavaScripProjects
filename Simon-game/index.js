db2495fd0f09229f0e85220cb48e7140eb3db616
Idrissa <idrisabkar2018@gmail.com>
2023-05-03 00:36:28 +0300

    simon

--- Simon-game/index.js
+++ Simon-game/index.js
@@ -1,5 +1,32 @@
-$(document).ready(function () {
-    const key = $(document).keydown(function (e) { 
-        $("h1").text(e.key);
-    });
+function randButton(){
+    const buttons = ["red","green","blue","white"]
+    const randomIndex = Math.floor(Math.random() * buttons.length);
+    const randomElement = buttons[randomIndex];
+    return randomElement
+}
+
+var clickedButtons = [];
+
+$(".square").click(function (e) { 
+    var button = $(e.target);
+    var id = button.attr("id");
+    const audio = new Audio("./sounds/" + id + ".mp3");
+    audio.play()
+    button.addClass("clicked");
+    setTimeout(() => {
+        button.removeClass("clicked");
+    }, 20);
+    
+    console.log(id);
+    if(clickedButtons.length != 0){
+        console.log(clickedButtons[0].attr("id"));
+    }
+});
+$(document).keydown(function (e) { 
+    if(e.key == "a"){ 
+       $("h1").text("Level 1");
+        var button = $("." + randButton());
+        button.click();
+        clickedButtons.push(button);
+    }
 });
