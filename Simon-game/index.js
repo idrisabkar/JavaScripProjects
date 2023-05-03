2a6f10c858660e0fe908d5a70f8584e1876e2f4f
Idrissa <idrisabkar2018@gmail.com>
2023-05-03 16:38:45 +0300

    saimon game finaltouches

--- Simon-game/index.js
+++ Simon-game/index.js
@@ -1,9 +1,18 @@
-$(document).ready(function () {
-    var buttonColours = ["red", "blue", "green", "white"];
+var buttonColours = ["red", "blue", "green", "white"];
 var userClickedPattern = []
 var gamePattern = [];
 var level = 0
 var started = false
+
+function startGame() {
+    $(document).keydown(function (e) { 
+        if(!started && e.key == "a"){
+            nextSequence()
+            started = true
+            $("h1").text("Level : " + level)
+        }
+    });
+}
 
 function nextSequence() {
   userClickedPattern = [];
@@ -15,6 +24,7 @@ function nextSequence() {
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound("sounds/" + randomChosenColour + ".mp3")
 }
+
 function playSound(sound){
     var audio = new Audio(sound);
     audio.play();
@@ -26,6 +36,7 @@ function animatePress(currentColour){
         $("#" + currentColour).removeClass("clicked");
     }, 100);
 }
+
 $(".square").click(function (e) { 
     var button = $(e.target)
     var userChosenColour = button.attr("id") 
@@ -35,18 +46,8 @@ $(".square").click(function (e) {
     checkAnswer(userClickedPattern.length - 1)
 });
 
-$(document).keydown(function (e) { 
-    if(!started){
-        nextSequence()
-        started = true
-        $("h1").text("Level : " + level)
-    }
-});
-
-
 function checkAnswer(currentLevel) {
     if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-        console.log("success")
       if (userClickedPattern.length === gamePattern.length) {
         userClickedPattern = [];
         setTimeout(function () {
@@ -54,8 +55,23 @@ function checkAnswer(currentLevel) {
         }, 1000);
       }
     } else {
-     console.log("wrong")
+     $("body").addClass("gameOver")
+     playSound("sounds/wrong.mp3");
+     setTimeout(() => {
+        $("body").removeClass("gameOver")
+     }, 500);
+        
+     gameOver()
     }
 }
 
+function gameOver(){
+    $("h1").text("Game Over, Press A to Restart")
+    level = 0
+    gamePattern = []
+    started = false   
+}
+
+$(document).ready(function () {
+ startGame()
 });
