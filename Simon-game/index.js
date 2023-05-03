cf650d270105d616f0c75be45aa96ea5c1d300ba
Idrissa <idrisabkar2018@gmail.com>
2023-05-03 14:18:36 +0300

    saimon game f

--- Simon-game/index.js
+++ Simon-game/index.js
@@ -1,19 +1,19 @@
-var buttonColours = ["red", "blue", "green", "white"];
+$(document).ready(function () {
+    var buttonColours = ["red", "blue", "green", "white"];
 var userClickedPattern = []
 var gamePattern = [];
 var level = 0
 var started = false
 
 function nextSequence() {
-level++
+  userClickedPattern = [];
+  level++
   $("h1").text("Level : " + level)
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound("sounds/" + randomChosenColour + ".mp3")
-  
-  
 }
 function playSound(sound){
     var audio = new Audio(sound);
@@ -30,9 +30,9 @@ $(".square").click(function (e) {
     var button = $(e.target)
     var userChosenColour = button.attr("id") 
     userClickedPattern.push(userChosenColour)
-    console.log(userClickedPattern)
     playSound("sounds/" + userChosenColour + ".mp3")
     animatePress(userChosenColour)
+    checkAnswer(userClickedPattern.length - 1)
 });
 
 $(document).keydown(function (e) { 
@@ -41,4 +41,21 @@ $(document).keydown(function (e) {
         started = true
         $("h1").text("Level : " + level)
     }
+});
+
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success")
+      if (userClickedPattern.length === gamePattern.length) {
+        userClickedPattern = [];
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+     console.log("wrong")
+    }
+}
+
 });
